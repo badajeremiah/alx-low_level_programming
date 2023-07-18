f68315094df1a189b7888325b925c7bacc5378e0
badajeremiah <badajeremiah@gmail.com>
2023-07-17 19:06:09 -0700

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0 once successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+
+		if (i >= 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
