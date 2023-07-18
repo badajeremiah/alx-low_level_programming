695cd5745340884b2d2b33be0b78f9698c463591
badajeremiah <badajeremiah@gmail.com>
2023-07-17 18:16:32 -0700

    Lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function prints the alphabeth in lowercase
+ *		followed bya new line using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
