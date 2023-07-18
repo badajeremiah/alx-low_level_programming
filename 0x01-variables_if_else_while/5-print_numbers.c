4f9910bc63e44caf7e49893ebdaaed4ad99dd889
badajeremiah <badajeremiah@gmail.com>
2023-07-17 18:50:20 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - e\entry point for program
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int b10;
+
+	for (b10 = 0; b10 < 10; b10++)
+	{
+		printf("%d", b10);
+	}
+
+	printf("\n");
+	return (0);
+}
