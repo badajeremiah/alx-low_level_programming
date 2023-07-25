8516a5d9aaccd9469c0f2f9cbf0a6091d440fe99
badajeremiah <badajeremiah@gmail.com>
2023-07-25 12:47:14 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LENGTH 10
+#define NUM_CHARS 62
+
+/**
+ * generate_password - Generate random passwords
+ *
+ * Return: void
+ */
+void generate_password() {
+	static char charset[] = "0123456789"
+                         "abcdefghijklmnopqrstuvwxyz"
+                         "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *password = malloc(LENGTH + 1);
+	int i;
+
+	if (password == NULL) 
+	{
+		return;
+	}
+	
+	for (i = 0; i < LENGTH; i++) 
+	{
+		int index = rand() % NUM_CHARS;
+		password[i] = charset[index];
+	}
+	
+	password[LENGTH] = '\0';
+
+	printf("%s\n", password);
+	
+	free(password);
+	
+}
+
+/**
+ * main - entry point to the program
+ *
+ * Return: void
+ * */
+int main(void) 
+{
+	srand(time(NULL));
+	
+	generate_password();
+	return (0);
+}
